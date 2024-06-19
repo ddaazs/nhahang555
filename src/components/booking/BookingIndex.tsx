6b254d4cecd0e7e84e0b19255d3b80123df1f5b9
AnhHoang <anhh9148@gmail.comgit config --global user.name AnhHoanggit config --global user.email anhh9148@gmail.com>
2024-06-19 22:58:41 +0700

    trans6

--- src/components/booking/BookingIndex.tsx
+++ src/components/booking/BookingIndex.tsx
@@ -14,46 +14,46 @@ const BookingIndex = () => {
                 </div>
                 <div className="col-md-6 bg-dark d-flex align-items-center">
                     <div className="p-5 wow fadeInUp" data-wow-delay="0.2s">
-                        <h5 className="section-title ff-secondary text-start text-primary fw-normal">Reservation</h5>
-                        <h1 className="text-white mb-4">Book A Table Online</h1>
+                        <h5 className="section-title ff-secondary text-start text-primary fw-normal">Đặt bàn</h5>
+                        <h1 className="text-white mb-4">Liên hệ đặt bàn </h1>
                         <form>
                             <div className="row g-3">
                                 <div className="col-md-6">
                                     <div className="form-floating">
                                         <input type="text" className="form-control" id="name" placeholder="Your Name" />
-                                        <label htmlFor='name'>Your Name</label>
+                                        <label htmlFor='name'>Tên</label>
                                     </div>
                                 </div>
                                 <div className="col-md-6">
                                     <div className="form-floating">
                                         <input type="email" className="form-control" id="email" placeholder="Your Email" />
-                                        <label htmlFor="email">Your Email</label>
+                                        <label htmlFor="email">Email</label>
                                     </div>
                                 </div>
                                 <div className="col-md-6">
                                     <div className="form-floating date" id="date3" data-target-input="nearest">
                                         <input type="text" className="form-control datetimepicker-input" id="datetime" placeholder="Date & Time" data-target="#date3" data-toggle="datetimepicker" />
-                                        <label htmlFor="datetime">Date & Time</label>
+                                        <label htmlFor="datetime">Ngày tháng</label>
                                     </div>
                                 </div>
                                 <div className="col-md-6">
                                     <div className="form-floating">
                                         <select className="form-select" id="select1">
-                                          <option value="1">People 1</option>
-                                          <option value="2">People 2</option>
-                                          <option value="3">People 3</option>
+                                          <option value="1">Một người</option>
+                                          <option value="2">Hai người</option>
+                                          <option value="3">Ba người</option>
                                         </select>
-                                        <label htmlFor="select1">No Of People</label>
+                                        <label htmlFor="select1">Số lượng người</label>
                                       </div>
                                 </div>
                                 <div className="col-12">
                                     <div className="form-floating">
                                         <textarea className="form-control" placeholder="Special Request" id="message" style={{ height: 100 }}></textarea>
-                                        <label htmlFor="message">Special Request</label>
+                                        <label htmlFor="message">Ghi chú</label>
                                     </div>
                                 </div>
                                 <div className="col-12">
-                                    <button className="btn btn-primary w-100 py-3" type="submit">Book Now</button>
+                                    <button className="btn btn-primary w-100 py-3" type="submit">Đặt</button>
                                 </div>
                             </div>
                         </form>
@@ -66,7 +66,7 @@ const BookingIndex = () => {
             <div className="modal-dialog">
                 <div className="modal-content rounded-0">
                     <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLabel">Youtube Video</h5>
+                        <h5 className="modal-title" id="exampleModalLabel">Video</h5>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
